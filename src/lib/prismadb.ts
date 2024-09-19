01e8c8ed9db45cd100998cea314c6d6537704fe3
Ezequiel Parziale <eparziale@gmail.com>
2024-09-19 19:47:45 -0300

    :card_file_box: feat: migrate to postgres db

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,13 +1,4 @@
-import { createClient } from "@libsql/client"
-import { PrismaLibSQL } from "@prisma/adapter-libsql"
 import { PrismaClient } from "@prisma/client"
-
-const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-})
-
-const adapter = new PrismaLibSQL(libsql)
 
 declare global {
   var prisma: PrismaClient | undefined
@@ -16,7 +7,6 @@ declare global {
 const prismadb =
   globalThis.prisma ||
   new PrismaClient({
-    adapter,
     log:
       process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   })
