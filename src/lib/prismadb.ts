1a090d85dacd0b01f01fb17d62ae8fce5e6da7d0
Ezequiel Parziale <eparziale@gmail.com>
2024-11-18 10:41:05 -0300

    :recycle: refactor: improve typescript code

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,15 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+const globalForPrisma = global as unknown as { prismadb: PrismaClient }
 
-const prismadb =
-  globalThis.prisma ||
+export const prismadb =
+  globalForPrisma.prismadb ||
   new PrismaClient({
     log:
       process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   })
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prismadb = prismadb
 
 export default prismadb
