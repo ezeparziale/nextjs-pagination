a6e83451c765c24d97f3bce465b49b1bfec22663
Ezequiel Parziale <eparziale@gmail.com>
2025-03-16 19:05:03 -0300

    :recycle: refactor: adjust navbar container class for improved layout

--- src/app/_components/navbar/navbar.tsx
+++ src/app/_components/navbar/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-muted bg-linear-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <nav
-        className="container flex h-16 max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8"
+        className="mx-auto w-full flex h-16 max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8"
         aria-label="Global"
       >
         <DesktopNav navigation={navigation} />
