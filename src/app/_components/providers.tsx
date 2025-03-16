7e3608b376dabfbfb5a2026cc3c97b9f3bac9881
Ezequiel Parziale <eparziale@gmail.com>
2025-03-16 19:31:18 -0300

    :recycle: refactor: add disableTransitionOnChange prop to ThemeProvider for improved theme switching

--- src/app/_components/providers.tsx
+++ src/app/_components/providers.tsx
@@ -4,7 +4,12 @@ import { ThemeProvider } from "next-themes"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       {children}
     </ThemeProvider>
   )
