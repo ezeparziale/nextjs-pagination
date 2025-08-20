2f51a5bc87ba4e23584cfd1bd52943771c70df70
Ezequiel Parziale <eparziale@gmail.com>
2025-08-20 18:47:06 -0300

    :wrench: chore: set new next config
    - enable typedRoutes
    - add image qualities

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "raw.githubusercontent.com" }],
+    qualities: [75, 100],
   },
+  typedRoutes: true,
 }
 
 export default nextConfig
