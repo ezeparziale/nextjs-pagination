9c2b543a84c12a054094bd76aef3e6e5fec8581c
Ezequiel Parziale <eparziale@gmail.com>
2025-12-04 19:17:32 -0300

    :rotating_light: chore: fix lint warning

--- src/app/_components/navbar/theme-switch.tsx
+++ src/app/_components/navbar/theme-switch.tsx
@@ -18,6 +18,7 @@ export default function ThemeSwitch() {
   const { setTheme } = useTheme()
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true)
   }, [])
 
