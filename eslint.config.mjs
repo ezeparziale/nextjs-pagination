dff80f63d92bbc1e200a463090dbc8ddc87eb361
Ezequiel Parziale <eparziale@gmail.com>
2025-12-04 19:17:24 -0300

    :rotating_light: chore: upgrade eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import { FlatCompat } from "@eslint/eslintrc"
-import { dirname } from "path"
-import { fileURLToPath } from "url"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import { defineConfig, globalIgnores } from "eslint/config"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
-  },
-]
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "src/generated/**",
+  ]),
+])
 
 export default eslintConfig
